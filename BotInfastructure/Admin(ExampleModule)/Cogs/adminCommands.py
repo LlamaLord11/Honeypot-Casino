ff3b356192430167643fbab991b6962da823ad25
LlamaLord121 <colebob.jordan@gmail.com>
2025-11-15 00:20:41 -0700

    Finished initial cogloader infastructure and created 2 basic admin commands

--- BotInfastructure/Admin(ExampleModule)/Cogs/adminCommands.py
+++ BotInfastructure/Admin(ExampleModule)/Cogs/adminCommands.py
@@ -10,28 +10,17 @@ class adminCommands(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-
-    # Makes the cog acessible to only people with admin role
-    async def interaction_check(self, interaction: discord.Interaction) -> bool:
-        user = interaction.guild.get_member(interaction.user.id)
-        userRoles = user.roles
-        if not any(role.id == ADMIN_ROLE_ID for role in userRoles):
-            print(f"non admin tried and failed to acess admin commands")
-            await interaction.response.send_message("Hey loser, you cant do that. Hence the big bold (Dev Only) description", ephemeral=True)
-            return False
-        return True
-
     @app_commands.command(name="shutdown", description="Shuts the bot down (Dev Only)")
     @app_commands.guilds(discord.Object(id=DEV_GUILD_ID))
     @app_commands.default_permissions(administrator=True)
-    async def shutOff(self, interaction: discord.Interaction):
+    async def shutDown(self, interaction: discord.Interaction):
         await interaction.response.send_message("Bot shutting down", ephemeral=True)
         await self.bot.close()
 
     @app_commands.command(name="list-commands", description="Lists currently loaded commands (Dev Only)")
     @app_commands.guilds(discord.Object(id=DEV_GUILD_ID))
     @app_commands.default_permissions(administrator=True)
-    async def showLoadedCommands(self, interaction: discord.Interaction):
+    async def listCommands(self, interaction: discord.Interaction):
         cmds = self.bot.tree.get_commands(guild=discord.Object(id=DEV_GUILD_ID))
         outputText = "=== Loaded Slash Commands ===\n"
 
